50cafa3f37eb83ef94d66d4ac184dda2480e6e9f
Vrylle <joshvryllecastillo@gmail.com>
2025-04-23 21:57:44 +0800

    Made dynamic URL and Content when pressing the buttons on USER DASHBOARD

--- DMS/dental/urls.py
+++ DMS/dental/urls.py
@@ -2,10 +2,26 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
+    # WEBSITE
     path('',views.website,name='website'),
-    path('testing/',views.testing, name='testing'),
-    path('UserDashboard/',views.user_dash,name='user_dash'),
-    path('DentistDashboard/',views.dentist_dash,name='dentist_dash'),
-    path('AdminDashboard/',views.admin_dash,name='admin_dash'),
+
+    # USER DASHBOARD
+    path('user/dashboard/',views.user_dash, name='user_dash'),
+    path('user/appointments/',views.user_appointments, name='user_appointments'),
+    path('user/analytics/',views.user_analytics, name='user_analytics'),
+    path('user/notifications/',views.user_notifications, name='user_notifications'),
+    path('user/prescription/',views.user_prescription, name='user_prescription'),
+    path('user/health-record/',views.user_health_record, name='user_health_record'),
+    path('user/emergency/',views.user_emergency, name='user_emergency'),
+    path('user/faq/',views.user_faq, name='user_faq'),
+
+    # DENTIST DASHBOARD
+    path('Dentist/',views.dentist_dash,name='dentist_dash'),
+
+    # ADMIN DASHBOARD
+    path('Admin/',views.admin_dash,name='admin_dash'),
     
+
+    # TESTING
+    path('testing/',views.testing, name='testing'),    
 ]
--- DMS/dental/views.py
+++ DMS/dental/views.py
@@ -3,19 +3,53 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import Patient
 
-
+# WEBSITE
 def website(request):
     return render(request, 'website.html')
 
+# USER
 def user_dash(request):
     return render(request, 'User/user_dash.html')
 
+# def user_landing(request):
+#     return render(request, 'User/user_landing.html')
+
+def user_appointments(request):
+    return render(request, 'User/user_appointments.html')
+
+def user_analytics(request):
+    return render(request, 'User/user_analytics.html')
+
+def user_notifications(request):
+    return render(request, 'User/user_notifications.html')
+
+def user_prescription(request):
+    return render(request, 'User/user_prescription.html')
+
+def user_health_record(request):
+    return render(request, 'User/user_health_record.html')
+
+def user_emergency(request):
+    return render(request, 'User/user_emergency.html')
+
+def user_faq(request):
+    return render(request, 'User/user_faq.html')
+
+
+
+
+# DENTIST
 def dentist_dash(request):
     return render(request, 'Dentist/dentist_dash.html')
 
+# ADMIN
 def admin_dash(request):
     return render(request, 'Admin/admin_dash.html')
 
+
+
+
+# TESTING
 def testing(request):
     testing_list = [1,2,3,4,5]
 
