12dd0b23ff09aee8e7a7588764eeede7f170f7d6
Vrylle <joshvryllecastillo@gmail.com>
2025-04-23 15:40:20 +0800

    Added User, Admin, Dentist Dashboard. Responsive Dashboards.

--- DMS/dental/views.py
+++ DMS/dental/views.py
@@ -8,13 +8,13 @@ def website(request):
     return render(request, 'website.html')
 
 def user_dash(request):
-    return render(request, 'user_dash.html')
+    return render(request, 'User/user_dash.html')
 
 def dentist_dash(request):
-    return render(request, 'dentist_dash.html')
+    return render(request, 'Dentist/dentist_dash.html')
 
 def admin_dash(request):
-    return render(request, 'admin_dash.html')
+    return render(request, 'Admin/admin_dash.html')
 
 def testing(request):
     testing_list = [1,2,3,4,5]
