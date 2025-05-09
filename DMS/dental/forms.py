f983da26c892d274927767fd64d4021f361e63e1
Vrylle <joshvryllecastillo@gmail.com>
2025-05-10 00:36:51 +0800

    May 10,2025, Fixed Admin Add Function. Added Full Patient Info Record.

--- DMS/dental/forms.py
+++ DMS/dental/forms.py
@@ -4,11 +4,129 @@ from .models import Patient
 class PatientForm(forms.ModelForm):
     class Meta:
         model = Patient
-        fields = ['first_name', 'middle_name', 'last_name','birth_date','age','religion','nationality','home_address','occupation','dental_insurance','dental_insurance_effective_date','sex']
+        fields = ['last_name',
+                  'first_name',
+                  'middle_name',
+                  'nickname',
+                  'birthdate',
+                  'age',
+                  'sex',
+                  'religion',
+                  'nationality',
+                  'home_address',
+                  'occupation',
+                  'dental_insurance',
+                  'dental_insurance_effective_date',
+                  'for_minors_parent_or_guardian_name',
+                  'for_minors_parent_or_guardian_occupation',
+                  'home_no',
+                  'office_no',
+                  'fax_no',
+                  'cel_mobile_no',
+                  'email',
+                  'referral_thanks',
+                  'dental_consultation_reason',
+                  'prev_dentist',
+                  'last_dental_visit',
+                  'physician_name',
+                  'physician_specialty',
+                  'office_address',
+                  'office_no',
+                  'mi_isgoodhealth',
+                  'mi_is_under_medical_treatment',
+                  'mi_is_under_medical_treatment_followup',
+                  'mi_is_serious_illness',
+                  'mi_is_serious_illness_followup',
+                  'mi_is_hospitalized',
+                  'mi_is_hospitalized_folloup',
+                  'mi_is_taking_prescription',
+                  'mi_is_taking_prescription_followup',
+                  'mi_is_using_tobacco',
+                  'mi_is_allergic',
+                  'mi_is_allergic_others',
+                  'mi_bleeding_time',
+                  'mi_is_pregnant',
+                  'mi_is_nursing',
+                  'mi_is_birth_control',
+                  'mi_bloodtype',
+                  'mi_bloodpressure',
+                  'email',
+
+                  ]
+
         widgets = {
-            'birth_date': forms.DateInput(attrs={'type': 'date'}),
+            'birthdate': forms.DateInput(attrs={'type': 'date'}),
             'dental_insurance_effective_date': forms.DateInput(attrs={'type': 'date'}),
+            'sex': forms.RadioSelect(),
+            'mi_isgoodhealth': forms.RadioSelect(),
+            'mi_is_under_medical_treatment': forms.RadioSelect(),
+            'mi_is_serious_illness': forms.RadioSelect(),   
+            'mi_is_hospitalized': forms.RadioSelect(),
+            'mi_is_taking_prescription': forms.RadioSelect(),
+            'mi_is_using_tobacco': forms.RadioSelect(),
+            'mi_is_allergic': forms.CheckboxSelectMultiple(),
+            'mi_is_pregnant': forms.RadioSelect(),
+            'mi_is_nursing': forms.RadioSelect(),
+            'mi_is_birth_control': forms.RadioSelect(),
         }
+
+    def __init__(self, *args, **kwargs):
+        super(PatientForm, self).__init__(*args, **kwargs)
+        
+        # Define custom labels for your fields
+        custom_labels = {
+            'last_name': 'Last Name',
+            'first_name': 'First Name',
+            'middle_name': 'Middle Name',
+            'nickname': 'Nickname',
+            'birthdate': 'Date of Birth',
+            'age': 'Age',
+            'sex': 'Sex',
+            'religion': 'Religion',
+            'nationality': 'Nationality',
+            'home_address': 'Home Address',
+            'occupation': 'Occupation',
+            'dental_insurance': 'Dental Insurance Provider',
+            'dental_insurance_effective_date': 'Insurance Effective Date',
+            'for_minors_parent_or_guardian_name': 'Parent/Guardian Name',
+            'for_minors_parent_or_guardian_occupation': 'Parent/Guardian Occupation',
+            'home_no': 'Home Phone Number',
+            'office_no': 'Office Phone Number',
+            'fax_no': 'Fax Number',
+            'cel_mobile_no': 'Mobile Number',
+            'email': 'Email Address',
+            'referral_thanks': 'Whom may we thank for referring you?',
+            'dental_consultation_reason': 'What is your reason for dental consultation?',
+            'prev_dentist': 'Previous Dentist: Dr.',
+            'last_dental_visit': 'Last Dental visit',
+            'physician_name': 'Physician Name',
+            'physician_specialty': 'Physician Specialty',
+            'office_address': 'Physician Office Address',
+            'mi_isgoodhealth': 'Are you in good health?',
+            'mi_is_under_medical_treatment': 'Under medical treatment?',
+            'mi_is_under_medical_treatment_followup': 'If yes, explain',
+            'mi_is_serious_illness': 'Any serious illness?',
+            'mi_is_serious_illness_followup': 'If yes, explain',
+            'mi_is_hospitalized': 'Have you been hospitalized?',
+            'mi_is_hospitalized_folloup': 'If yes, explain',
+            'mi_is_taking_prescription': 'Taking any prescriptions?',
+            'mi_is_taking_prescription_followup': 'If yes, explain',
+            'mi_is_using_tobacco': 'Do you use tobacco?',
+            'mi_is_allergic': 'Allergies (Check all that apply)',
+            'mi_is_allergic_others': 'Other allergies (if any)',
+            'mi_bleeding_time': 'Bleeding Time',
+            'mi_is_pregnant': 'Are you pregnant?',
+            'mi_is_nursing': 'Are you nursing?',
+            'mi_is_birth_control': 'Using birth control?',
+            'mi_bloodtype': 'Blood Type',
+            'mi_bloodpressure': 'Blood Pressure',
+        }
+
+        #  Apply the custom labels
+        for field_name, label in custom_labels.items():
+            if field_name in self.fields:
+                self.fields[field_name].label = label
+
 
 class ConsentForm(forms.Form):
     consent_signed = forms.BooleanField(label="Patient has signed digital informed consent.")
--- DMS/dental/models.py
+++ DMS/dental/models.py
@@ -55,33 +55,136 @@ class DentistProfile(models.Model):
 
 # PATIENT
 class Patient(models.Model):
-    first_name = models.CharField(max_length=100, null=False)
-    middle_name = models.CharField(max_length=100, null=True)
+    # Patient Information Record
     last_name = models.CharField(max_length=100, null=False)
-    birth_date = models.DateField( null=False)
+    first_name = models.CharField(max_length=100, null=False)
+    middle_name = models.CharField(max_length=100, null=False)
+    nickname = models.CharField(max_length=100,null=True, blank=True)
+    birthdate = models.DateField( null=False)
     age = models.PositiveIntegerField(default=0,null=False)
-    religion = models.CharField(max_length=100, null=True)
-    nationality = models.CharField(max_length=100, null=True)
-    home_address = models.CharField(max_length=255, null=True)
-    occupation = models.CharField(max_length=100, null=True)
-    dental_insurance = models.CharField(max_length=254, null=True, blank=True)
-    dental_insurance_effective_date = models.DateField(null=True, blank=True)
-
     sex_choices = [
         ('M','Male'),
         ('F','Female'),
     ]
-
-    sex = models.CharField(max_length=1, choices=sex_choices, null=True)
-
-
-
+    sex = models.CharField(max_length=10, choices=sex_choices, null=False, default=None)
+    religion = models.CharField(max_length=100, null=False)
+    nationality = models.CharField(max_length=100, null=False)
+    home_address = models.CharField(max_length=255, null=False)
+    occupation = models.CharField(max_length=100, null=False)
+    dental_insurance = models.CharField(max_length=254, null=True, blank=True)
+    dental_insurance_effective_date = models.DateField(null=True, blank=True)
 
     # For Minors
-    # parent_or_guardian_name = models.CharField(max_length=255)
-    # parent_or_guardian_occupation = models.CharField(max_length=100)
-    # referral_thanks = models.CharField(max_length=255)
-    # dental_consultation_reason = models.CharField(max_length=255)
+    for_minors_parent_or_guardian_name = models.CharField(max_length=255, null=False)
+    for_minors_parent_or_guardian_occupation = models.CharField(max_length=100, null=False)
+
+    # Contacts
+    home_no = models.CharField(max_length=255, null=True, blank=True)
+    office_no = models.CharField(max_length=255, null=True, blank=True)
+    fax_no = models.CharField(max_length=255, null=True, blank=True)
+    cel_mobile_no = models.IntegerField(null=False)
+    email = models.CharField(max_length=100, null=False)
+
+
+    # Referral Thanks
+    referral_thanks = models.CharField(max_length=255, null=True, blank=True)
+    dental_consultation_reason = models.CharField(max_length=255, null=True, blank=True)
+
+    # Dental History 
+    prev_dentist = models.CharField(max_length=255, null=True, blank=True)
+    last_dental_visit = models.CharField(max_length=255, null=True, blank=True)
+
+    # Medical History 
+    physician_name = models.CharField(max_length=255, null=True, blank=True)
+    physician_specialty = models.CharField(max_length=255, null=True, blank=True)
+    office_address = models.CharField(max_length=255, null=True, blank=True)
+    office_no = models.CharField(max_length=255, null=True, blank=True)
+    
+    # Medical Information
+    yes_no = [
+        ('YES','Yes'),
+        ('NO','No'),
+    ]
+    mi_isgoodhealth = models.CharField(max_length=255, choices=yes_no, default=None, null=False)
+
+    mi_is_under_medical_treatment = models.CharField(max_length=255, choices=yes_no, default=None, null=False)
+    mi_is_under_medical_treatment_followup = models.CharField(max_length=255, null=True, blank=True)
+
+    mi_is_serious_illness = models.CharField(max_length=255 , choices=yes_no, default=None, null=False)
+    mi_is_serious_illness_followup = models.CharField(max_length=255, null=True, blank=True)
+
+    mi_is_hospitalized = models.CharField(max_length=255, choices=yes_no, default=None, null=False)
+    mi_is_hospitalized_folloup = models.CharField(max_length=255, null=True, blank=True)
+
+    mi_is_taking_prescription = models.CharField(max_length=255, choices=yes_no, default=None, null=False)
+    mi_is_taking_prescription_followup = models.CharField(max_length=255, null=True, blank=True)
+
+    mi_is_using_tobacco = models.CharField(max_length=255, choices=yes_no, default=None, null=False)
+
+    allergies = [
+        ('LOCAL ANESTHETIC','Local Anesthetic (ex. Lidocaine)'),
+        ('PENICILIN, ANTIBIOTICS','Penicilin, Antibiotics'),
+        ('SULFA DRUGS','Sulfa Drugs'),
+        ('ASPIRIN','Aspirin'),
+        ('LATEX','Latex'),
+        ('OTHERS','Others')
+    ]
+    mi_is_allergic = models.JSONField(default=list, choices=allergies, null=True, blank=True)
+    mi_is_allergic_others = models.CharField(max_length=255, null=True, blank=True)
+
+    mi_bleeding_time = models.CharField(max_length=255, blank=True, null=True)
+
+    # MI For Women Only
+    mi_is_pregnant = models.CharField(max_length=255, choices=yes_no, default=None, null=True,blank=True)
+    mi_is_nursing = models.CharField(max_length=255, choices=yes_no, default=None, null=True, blank=True)
+    mi_is_birth_control = models.CharField(max_length=255, choices=yes_no, default=None, null=True, blank=True)
+    
+    mi_bloodtype = models.CharField(max_length=255, null=False)
+    mi_bloodpressure = models.CharField(max_length=255, null=False)
+
+    diseases = [
+        ('HIGH BLOOD PRESSURE','High Blood Pressure'),
+        ('LOW BLOOD PRESSURE','Low Blood Pressure'),
+        ('EPILEPSY/CONVULSIONS','Epilepsy / Conculsions'),
+        ('AIDS OR HIV INFECTION','AIDS or HIV Infection'),
+        ('SEXUALLY TRANSMITTED DISEASE','Sexually Transmitted Disease'),
+        ('STOMACH TROUBLES / ULCERS','Stomach Troubles / Ulcers'),
+        ('FAINTING SEIZURES','Fainting Seizures'),
+        ('RAPID WEIGHT LOSS','Rapid Weight Loss'),
+        ('RADIATION THERAPY','Radiation Therapy'),
+        ('JOINT REPLACEMENT / IMPLANT','Joint Replacement / Implant'),
+        ('HEART SURGERY','Heart Surgery'),
+        ('HEART ATTACK','Heart Attack'),
+        ('THYROID PROBLEM','Thyroid Problem'),
+        ('HEART DISEASE','Heart Disease'),
+        ('HEART MURMUR','Heart Murmur'),
+        ('HEPATITIS / LIVER DISEASE','Hepatitis / Liver Disease'),
+        ('RHEUMATIC FEVER','Rheumatic Fever'),
+        ('HAY FEVER / ALLERGIES','Hay Fever / Allergies'),
+        ('RESPIRATORY PROBLEMS','Respiratory Problems'),
+        ('HEPATITIS / JAUNDICE','Hepatitis / Jaundice'),
+        ('TUBERCULOSIS','Tuberculosis'),
+        ('SWOLLEN ANKLES','Swollen Ankles'),
+        ('KIDNEY DISEASE','Kidney Disease'),
+        ('DIABETES','Diabetes'),
+        ('CHEST PAIN','Chest Pain'),
+        ('STROKE','Stroke'),
+        ('CANCER / TUMORS','Cancer / Trumors'),
+        ('ANEMIA','Anemia'),
+        ('ANGINA','Angina'),
+        ('ASTHMA','Asthma'),
+        ('EMPHYSEMA','Empysema'),
+        ('BLEEDING PROBLEMS','Bleeding Problems'),
+        ('BLOOD DISEASES','Blood Diseases'),
+        ('HEAD INJURIES','Head Injuries'),
+        ('ARTHRITIS / RHEUMATISM','Arthritis / Rheumatism'),
+    ]
+
+    mi_select_disease = models.JSONField(default=list, choices=diseases, null=True, blank=True)
+    mi_select_disease_others = models.CharField(max_length=255, null=True, blank=True)
+
+
+
 
 
 
